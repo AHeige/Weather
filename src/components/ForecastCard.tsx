3de3c5d74c007c44e6b098e3f7984df3e5736979
Aheige <aheige17@gmail.com>
2022-06-22 22:09:14 +0200

    Date fix

--- src/components/ForecastCard.tsx
+++ src/components/ForecastCard.tsx
@@ -1,31 +1,31 @@
-import React from "react";
-import ScrollContainer from "react-indiana-drag-scroll";
+import React from "react"
+import ScrollContainer from "react-indiana-drag-scroll"
 
 //utils
-import { resolveForecastData } from "../utils/forecast";
+import { resolveForecastData } from "../utils/forecast"
 
 //components
-import Card from "@mui/material/Card";
-import CardHeader from "@mui/material/CardHeader";
-import CardContent from "@mui/material/CardContent";
-import Grid from "@mui/material/Grid";
+import Card from "@mui/material/Card"
+import CardHeader from "@mui/material/CardHeader"
+import CardContent from "@mui/material/CardContent"
+import Grid from "@mui/material/Grid"
 
 const ForecastCard = (forecast: any) => {
-  const list = resolveForecastData(forecast);
+  const list = resolveForecastData(forecast)
 
-  const forecastlist = Object.values(list)[0];
+  const forecastlist = Object.values(list)[0]
 
   const forecastTable = () => {
     return forecastlist.map((a: any) => (
       <Grid item xs={3} direction={"column"} key={a.key}>
         <Card style={{ backgroundColor: `rgb(255,255,255, 0)` }}>
-          <CardHeader title={a.day} subheader={a.time} />
+          <CardHeader title={a.day.toString()} subheader={a.time.toString()} />
           <CardContent>{a.desc}</CardContent>
           <CardContent>{a.temp}</CardContent>
         </Card>
       </Grid>
-    ));
-  };
+    ))
+  }
 
   return (
     <ScrollContainer horizontal hideScrollbars>
@@ -37,12 +37,11 @@ const ForecastCard = (forecast: any) => {
           flexGrow: 1,
           gridColumn: 1,
           marginTop: "0em",
-        }}
-      >
+        }}>
         {forecastTable()}
       </Grid>
     </ScrollContainer>
-  );
-};
+  )
+}
 
-export default ForecastCard;
+export default ForecastCard
